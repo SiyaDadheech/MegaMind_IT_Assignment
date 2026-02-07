27d1b71dcb30b9fddc98f33472bd674359e29288
Siya Dadheech <162879979+SiyaDadheech@users.noreply.github.com>
2026-02-07 10:43:32 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -14,3 +14,13 @@ resource "aws_iam_role" "lambda_role" {
     ]
   })
 }
+# IMPORTANT: Permissions attach karna mat bhoolna (S3, Logs, SES ke liye)
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
